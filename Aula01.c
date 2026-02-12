b3ed1d982bd858fe5515a28231a08fef9646f470
GiovaniCestari <giocestaricba@gmail.com>
2026-02-12 17:18:55 -0300

    Create Aula01.c

---
+++ Aula01.c
@@ -0,0 +1,13 @@
+/* Lab. 1
+  * Exemplo 1 */
+
+#include <stdio.h>
+
+int main() {
+  // Imprime algumas mensagens na tela
+printf("Mackenzie - FCI \n");
+printf("Algoritmos e ");
+printf("Prog. II\n");
+
+return 0;
+}
